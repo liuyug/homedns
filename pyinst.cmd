bb4df2d0c695f8d21a14363aa2d8693e5294b5be
U-IBM194-P16TCDD\liuyug <liuyug@gmail.com>
2016-02-05 16:35:55 +0800

    copy rules file

--- pyinst.cmd
+++ pyinst.cmd
@@ -2,5 +2,8 @@
 
 rmdir build /s /q
 
-pyinstaller --clean --noupx -c -p homedns -F homedns.py
+pyinstaller --clean --noupx -y -c -p homedns -F homedns.py
 
+copy default.rules dist /y
+copy black.rules dist /y
+copy hosts.homedns dist /y
