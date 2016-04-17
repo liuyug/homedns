b89256067856c3a720211f1953ae5c7bcd1ab9f3
U-IBM194-P16TCDD\liuyug <liuyug@gmail.com>
2016-04-17 20:59:20 +0800

    add dnsresolver script

---
+++ dnsresolver.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+
+from homedns.scripts.dnsresolver import main
+
+main()
--- pack.cmd
+++ pack.cmd
@@ -6,5 +6,6 @@ python setup.py build_ext --inplace
 
 pyinstaller --icon App-browser.ico --clean --noupx --noconfirm --console --paths homedns --onefile hdns.py
 pyinstaller --icon App-browser.ico --clean --noupx --noconfirm --console --paths homedns --onefile dnsprobe.py
+pyinstaller --icon App-browser.ico --clean --noupx --noconfirm --console --paths homedns --onefile dnsresolver.py
 
 rem pyinstaller --icon App-browser.ico --clean --noupx --noconfirm --console --paths homedns hdns.py dnsprobe.py
