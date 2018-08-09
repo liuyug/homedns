42ec04def2721611c44d87cd60e30ab98c44040a
LIU Yugang <liuyug@gmail.com>
2018-08-09 10:55:17 +0800

    fix

--- win32/pack.cmd
+++ win32/pack.cmd
@@ -3,7 +3,7 @@
 cls
 
 if x%1 == x (
-    echo %0 [hdns, dnsprobe, dnsresolver]
+    echo %0 [hdns, dnsprobe]
     exit /b
 )
 
@@ -18,10 +18,6 @@ if x%1 == xhdns (
 
 if x%1 == xdnsprobe (
     pyinstaller --icon App-browser.ico --clean --noupx --noconfirm --console --paths homedns --onefile dnsprobe.py
-)
-
-if x%1 == xdnsresolver (
-    pyinstaller --icon App-browser.ico --clean --noupx --noconfirm --console --paths homedns --onefile dnsresolver.py
 )
 
 del *.spec /q
